a93f9b0a435d05165e7947f93248fcb52b01fe88
aycanogut <aycanonfire@gmail.com>
2021-11-06 19:51:48 +0300

    update card render method

--- src/components/Cards/Cards.styled.jsx
+++ src/components/Cards/Cards.styled.jsx
@@ -22,48 +22,21 @@ const StyledCards = styled.section`
 `;
 
 const Cards = ({ weather }) => {
+  const days = Object.values(weather);
+
   return (
     <StyledCards>
-      <Card
-        day={weather.dayOne.weekday}
-        date={weather.dayOne.date}
-        location={weather.dayOne.location}
-        temperature={weather.dayOne.temperature}
-        weatherIcon={weather.dayOne.weatherIcon}
-        weatherDescription={weather.dayOne.weatherDescription}
-      />
-      <Card
-        day={weather.dayTwo.weekday}
-        date={weather.dayTwo.date}
-        location={weather.dayTwo.location}
-        temperature={weather.dayTwo.temperature}
-        weatherIcon={weather.dayTwo.weatherIcon}
-        weatherDescription={weather.dayTwo.weatherDescription}
-      />
-      <Card
-        day={weather.dayThree.weekday}
-        date={weather.dayThree.date}
-        location={weather.dayThree.location}
-        temperature={weather.dayThree.temperature}
-        weatherIcon={weather.dayThree.weatherIcon}
-        weatherDescription={weather.dayThree.weatherDescription}
-      />
-      <Card
-        day={weather.dayFour.weekday}
-        date={weather.dayFour.date}
-        location={weather.dayFour.location}
-        temperature={weather.dayFour.temperature}
-        weatherIcon={weather.dayFour.weatherIcon}
-        weatherDescription={weather.dayFour.weatherDescription}
-      />
-      <Card
-        day={weather.dayFive.weekday}
-        date={weather.dayFive.date}
-        location={weather.dayFive.location}
-        temperature={weather.dayFive.temperature}
-        weatherIcon={weather.dayFive.weatherIcon}
-        weatherDescription={weather.dayFive.weatherDescription}
-      />
+      {days.map((day, index) => (
+        <Card
+          key={index}
+          day={day.weekday}
+          date={day.date}
+          location={day.location}
+          temperature={day.temperature}
+          weatherIcon={day.weatherIcon}
+          weatherDescription={day.weatherDescription}
+        />
+      ))}
     </StyledCards>
   );
 };
